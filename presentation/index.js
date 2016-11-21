335e591bf82f3058a4431d6ef8149f1afed59eb8
Eric Stroczynski <ericstroczynski@gmail.com>
2016-11-20 20:00:11 -0800

    Updated slides, final draft

--- presentation/index.js
+++ presentation/index.js
@@ -74,11 +74,11 @@ export default class Presentation extends React.Component {
             <Heading fit size={4} textColor="primary">
               Motivations and Data
             </Heading>
-            <List>
-              <ListItem>As developers, we want to help end users</ListItem>
-              <ListItem>We can improve products through data</ListItem>
-              <ListItem>Requests data = City of Surrey "product" data</ListItem>
+            <List style={{marginLeft: 64}}>
+              <ListItem>A developers goal: <b>help end users</b></ListItem>
+              <ListItem>How can we improve products? Using <b>data</b>!</ListItem>
               <ListItem>Iterative feedback cycles build robust systems</ListItem>
+              <ListItem><u>Requests data</u> is City of Surrey "product" data</ListItem>
             </List>
           </Slide>
 
@@ -92,11 +92,14 @@ export default class Presentation extends React.Component {
           </Slide>
 
           <Slide transition={["spin"]} bgColor="tertiary">
+          <Heading fit size={1} textColor="primary">
+            The Requests System
+          </Heading>
             <List>
               <ListItem>Citizens of Surrey, the user, make categorized requests</ListItem>
-              <ListItem>60,000+ requests made in 2016, 57,000 (>91%) closed</ListItem>
+              <ListItem><b>60,000+ requests</b> so far in 2016, <b>>91% closed</b></ListItem>
               <ListItem>70,000 requests per year from 2013-2015</ListItem>
-              <ListItem>Many requests, active system -> inefficiencies??</ListItem>
+              <ListItem>Many requests, active system <b>=></b> inefficiencies??</ListItem>
             </List>
           </Slide>
 
@@ -105,105 +108,117 @@ export default class Presentation extends React.Component {
               Relevant Questions
             </Heading>
             <List>
-              <ListItem>What is the average close time for a request?</ListItem>
-              <ListItem>Which agencies get the most traffic</ListItem>
-              <ListItem>Are there duplicates in the data? Frequency? Importance?</ListItem>
-              <ListItem>How do users know their requests have been heard?</ListItem>
+              <ListItem>What is the <b>average close time</b> for a request?</ListItem>
+              <ListItem>Which agencies get the <b>most traffic</b>?</ListItem>
+              <ListItem>Are there <b>duplicates</b> in the data? Frequency? Importance?</ListItem>
+              <ListItem>How do users <b>interact</b> with the requests system?</ListItem>
             </List>
           </Slide>
 
           <Slide transition={["spin"]} bgColor="tertiary">
-            <Heading fit size={4} textColor="primary">
-              Some Answers
+            <Heading size={1} textColor="primary">
+              Relevant Answers
             </Heading>
-            <List>
-              <ListItem>Average close time: 9 days</ListItem>
-              <ListItem>70% of all issues since 2010: Litter, Traffic, Trees</ListItem>
-              <ListItem>Over 700 uncategorized requests in 2016 alone</ListItem>
-              <ListItem>Some requests had up to 52 duplicates</ListItem>
+            <List style={{marginLeft: 64}}>
+              <ListItem>Average close time: <b>9 days</b></ListItem>
+              <ListItem>70% of all issues since 2010: <b>Litter</b>, <b>Traffic</b>, <b>Trees</b></ListItem>
+              <ListItem>Over <b>700</b> uncategorized requests in 2016 alone</ListItem>
+              <ListItem>Some requests had up to <b>52 duplicates</b></ListItem>
             </List>
           </Slide>
 
           <Slide transition={["spin"]} bgColor="tertiary">
-            <Heading fit size={4} textColor="primary">Problems</Heading>
+            <Heading size={1} textColor="primary">Problems</Heading>
             <List>
-              <ListItem>Inefficiencies caused by duplicates and uncategorized</ListItem>
-              <ListItem>Transparency and Community Engagement</ListItem>
+              <ListItem>Inefficiencies caused by duplicates and uncategorized data</ListItem>
+              <ListItem>Potential for transparency and community engagement</ListItem>
             </List>
           </Slide>
 
           <Slide transition={["spin"]} bgColor="tertiary">
-            <Heading fit size={1} textColor="primary">Solutions</Heading>
+            <Heading size={1} textColor="primary">Solutions</Heading>
+            <br/>
             <Heading fit size={2}><u>Research Goal:</u> Improve efficacy of City of Surrey Request system</Heading>
             <List>
               <ListItem>Target duplicates for consolidation</ListItem>
               <ListItem>Cross validate requests with Twitter data</ListItem>
               <ListItem>Category suggestion algorithm for requests</ListItem>
-              <ListItem>Update expected response times on requests in real time</ListItem>
+              <ListItem>Frequently update expected response times on requests</ListItem>
             </List>
           </Slide>
 
           <Slide bgColor="primary">
-            <Heading fit size={4} textColor="tertiary">
+            <Heading size={1} textColor="tertiary">
               Classification
             </Heading>
             <RequestClassifier />
           </Slide>
 
           <Slide bgColor="primary">
-            <Heading fit size={4} textColor="tertiary">
+            <Heading size={1} textColor="tertiary">
               Suggestions
             </Heading>
             <UserForm />
           </Slide>
 
           <Slide transition={["spin"]} bgColor="primary">
-            <Heading fit size={4} textColor="tertiary">
+            <Heading fit size={1} textColor="tertiary">
               Consolidate Duplicates and Tweets
             </Heading>
               <CityDashboard />
           </Slide>
 
-          <Slide bgColor="primary">
-            <Heading fit size={4} textColor="tertiary">
+          <Slide bgColor="tertiary">
+            <Heading fit size={4} textColor="primary">
               Expected Time Updates
             </Heading>
-            <List>
+            <List style={{marginLeft: 32}}>
               <ListItem>Calculate moving average over 5 day periods</ListItem>
               <ListItem>Weights fresh data more heavily</ListItem>
-              <ListItem>Shows active involvement with city</ListItem>
+              <ListItem>Shows active involvement, enhance <b>user experience</b></ListItem>
             </List>
           </Slide>
 
-          <Slide bgColor="primary">
-            <Heading fit size={4} textColor="tertiary">
-              Material Savings
+          <Slide bgColor="tertiary">
+            <Heading fit size={1} textColor="primary">
+              Effects of Optimization
+            </Heading>
+            <List style={{marginLeft: 48}}>
+              <ListItem>Automated re-routing: <b>> $20,000/year</b> (manual)</ListItem>
+              <ListItem>Consolidation: <b>> $2,500/year/employee</b></ListItem>
+              <ListItem>Community engagement: <b>priceless!</b></ListItem>
+            </List>
+          </Slide>
+
+          <Slide bgColor="tertiary">
+            <Heading size={1} textColor="primary">
+              Conclusion
             </Heading>
             <List>
-              <ListItem>OTHER re-routing: > $20,000/year (clerk at minimum wage)</ListItem>
-              <ListItem>Consolidation: > 1 hour/day (manual consolidation)</ListItem>
-              <ListItem>Community engagement: priceless</ListItem>
+              <ListItem><b>Statistics</b> can enhance UX and optimize Requests system</ListItem>
+              <ListItem><b>Tools</b> can improve information exchange between parties</ListItem>
+              <ListItem>Utilizing user feedback <b>aligns goals</b> of producer and user</ListItem>
             </List>
           </Slide>
 
-          <Slide bgColor="primary">
-            <Heading fit size={4} textColor="tertiary">
+          <Slide bgColor="tertiary">
+            <Heading size={1} textColor="primary">
               Technologies
             </Heading>
-            <List>
-              <ListItem>Web hosting: Bluemix</ListItem>
-              <ListItem>Analysis: TextBlob, Scikit Learn (Python)</ListItem>
-              <ListItem>Slides: ReactJS (JavaScript)</ListItem>
+            <List style={{marginLeft: 96}}>
+              <ListItem><u>Web hosting (slides, API):</u> Bluemix</ListItem>
+              <ListItem><u>Analysis:</u> TextBlob, Scikit Learn (Python)</ListItem>
+              <ListItem><u>Slides:</u> ReactJS (JavaScript)</ListItem>
             </List>
           </Slide>
 
-          <Slide bgColor="primary">
-            <Heading fit size={4} textColor="tertiary">
+          <Slide bgColor="tertiary">
+            <Heading size={1} textColor="#4f90ff">
               Acknowledgements
             </Heading>
             <List>
               <ListItem>BlueMix Team</ListItem>
-              <ListItem>Alan Devine, Ranjita Ravi, Behdad Jamshidi</ListItem>
+              <ListItem>Allen Devine, Ranjita Ravi, Behdad Jamshidi</ListItem>
               <ListItem>Telus, IEEE, and City of Surrey</ListItem>
             </List>
           </Slide>
